5eb9bddd4ad0845238591d07c7cd2c549d3b6830
Richard Kadey <richardkadey@gmail.com>
2024-05-28 05:58:31 +0000

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="rkadey",
+    author_email="richardkadey@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
